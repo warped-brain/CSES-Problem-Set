c56ee296901ccb17295d0d5fcb7d5e1bc436ab9c
Warped-Brain <110997347+warped-brain@users.noreply.github.com>
2025-08-06 14:22:57 +0530

    Add solutions for dynamic programming problems
    Added solutions for coin combination i, dice combinations and minimizing coins

---
+++ dynamic_programming/src/coin_combinations_I.rs
@@ -0,0 +1,151 @@
+use std::cmp::{max, min};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque};
+use std::fmt::write;
+use std::fs::read;
+use std::i64::MAX;
+use std::io::{self, BufRead, BufReader, StdinLock, BufWriter, StdoutLock}; // Import necessary modules
+use std::io::Write;
+use std::num::NonZeroU64;
+use std::ops::Bound;
+use std::{u64, usize};
+fn read_int(reader: &mut BufReader<StdinLock<'static>>) -> i32 {
+    let mut line = String::new();
+    reader.read_line(&mut line)
+    .expect("Failed to read N");
+
+    let n: i32 = line.trim().parse().unwrap();
+    n
+}
+fn read_int_pair(reader: &mut BufReader<StdinLock<'static>>) -> (i32, i32) {
+    let mut line = String::new();
+    reader.read_line(&mut line)
+    .expect("Failed to read N");
+    let mut iter = line.split(" ").into_iter();
+    let n: i32 = iter.next().unwrap().trim().parse().unwrap();
+    let n1 = iter.next().unwrap().trim().parse().unwrap();
+    (n, n1)
+}
+fn read_int_triplet(reader: &mut BufReader<StdinLock<'static>>) -> (i32, i32, i32) {
+    let mut line = String::new();
+    reader.read_line(&mut line)
+    .expect("Failed to read N");
+    let mut iter = line.split(" ").into_iter();
+    let n: i32 = iter.next().unwrap().trim().parse().unwrap();
+    let n1 = iter.next().unwrap().trim().parse().unwrap();
+    let n2 = iter.next().unwrap().trim().parse().unwrap();
+    (n, n1, n2)
+}
+fn read_string(reader: &mut BufReader<StdinLock<'static>>) -> String {
+    let mut line = String::new();
+    reader.read_line(&mut line)
+    .expect("Failed to read N");
+    line
+}
+fn write_int(writer: &mut BufWriter<StdoutLock<'static>>, n : i32) {
+    let _ = write!(writer, "{n}");
+}
+fn write_string(writer: &mut BufWriter<StdoutLock<'static>>, word: String) {
+    let _ = write!(writer, "{}", word);
+}
+fn write_string_list(writer: &mut BufWriter<StdoutLock<'static>>, list: Vec<String>) {
+    for word in list{
+        let _ = writeln!(writer, "{}", word);
+    }
+}
+fn write_u64(writer: &mut BufWriter<StdoutLock<'static>>, n : u64) {
+    let _ = write!(writer, "{n}");
+}
+fn read_int_list(reader: &mut BufReader<StdinLock<'static>>) -> Vec<i32>{
+    
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    let list = line.split_ascii_whitespace().map(|x| x.parse().unwrap()).collect();
+    list
+
+}
+
+fn read_uint64_list(reader: &mut BufReader<StdinLock<'static>>) -> Vec<u64>{
+    
+    let mut line = String::new();   
+    reader.read_line(&mut line).unwrap();
+    let list = line.split_ascii_whitespace().map(|x| x.parse().unwrap()).collect();
+    list
+
+}
+
+fn lower_bound(list: &Vec<i32>, elem: i32) -> usize{
+    let mut left:i32 = 0;
+    let mut right:i32 = list.len() as i32 - 1;
+    let mut mid:i32 = 0;
+    let mut ans = list.len() -1;
+    while(left <= right){
+        mid = left + ((right - left) >> 1);
+        if list[mid as usize] > elem {
+            ans = mid as usize;
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    // println!("ans {}", ans);
+    ans as usize
+}
+
+fn rec(sum:i32, coins: &Vec<i32>, dp: &mut Vec<i64>) -> Option<u64>{
+    if sum == 0 {
+        return Some(0);
+    }
+    if sum < 0 {
+        return None;
+    }
+    if dp[sum as usize] != -1 {
+        return Some(dp[sum as usize] as u64);
+    }
+    let mut ans = None;
+    for coin in coins.iter(){
+        if let Some(count) = rec(sum - *coin, coins, dp){
+            ans = Some(ans.unwrap_or(u64::MAX).min(count + 1));
+        }
+        
+    }
+    if let Some(val)= ans{
+        dp[sum as usize] = val as i64;
+    }
+    return ans;
+}
+
+const MOD:i64 = 1e9 as i64 +7;
+
+fn testcase(reader:&mut BufReader<StdinLock<'static>> , writer: &mut BufWriter<StdoutLock<'static>>) {
+    let (n, sums) = read_int_pair(reader);
+    let mut coins = read_int_list(reader);
+    let mut dp = vec![0; sums as usize+1];
+    dp[0] = 1;
+    for s in 1..=sums as usize{
+        for coin in coins.iter(){
+            if s as i32 - *coin  >= 0{
+                if dp[s - *coin as usize] != -1 {
+                    dp[s] = (dp[s] + dp[s - *coin as usize]) % MOD;
+                }
+                
+            }
+        }
+    }
+    println!("{:?}", dp.last().unwrap());
+}
+
+
+fn main() {
+    let stdin = io::stdin(); // Get a handle to standard input
+    let mut reader = io::BufReader::new(stdin.lock()); // Create a buffered reader
+    let stdout = io::stdout();
+    let mut writer = io::BufWriter::new(stdout.lock());
+    // let t = read_int(&mut reader);
+    // for _ in 0..t{
+        testcase(&mut reader,&mut writer);
+            // println!("NO");
+        // };
+    // }
+    
+}   
